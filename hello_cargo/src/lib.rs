76183cc0fe4609512a1ca8406fafeac970f196ed
marques-rick <hmservicostech@outlook.com.br>
2024-03-12 20:03:32 -0300

    using dependencies cargo

--- hello_cargo/src/lib.rs
+++ hello_cargo/src/lib.rs
@@ -1,5 +1,14 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 //by default, all functions are private
 //needed to put the word 'pub' to make it public
 pub fn greet() {
-    println!("Hello world!")
+    let stdout = stdout();
+    let message = String::from("Hello world!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+
+    let _ = say(&message, width, &mut writer);
 }
