4a47083873d2ccf3cc15bcc52f68e8f221014703
marques-rick <hmservicostech@outlook.com.br>
2024-03-16 12:44:50 -0300

    created get all and get by id method

--- rust_api/src/services.rs
+++ rust_api/src/services.rs
@@ -1,12 +1,17 @@
 use actix_web::{
     get, post,
-    web::{scope, Data, Json, ServiceConfig},
+    web::{scope, Data, Json, Path, Query, ServiceConfig},
     HttpResponse, Responder,
 };
 
 use serde_json::json;
+use uuid::{uuid, Uuid};
 
-use crate::{model::TaskModel, schema::CreateTaskSchema, AppState};
+use crate::{
+    model::TaskModel,
+    schema::{CreateTaskSchema, FilterOptions},
+    AppState,
+};
 
 #[get("/healthchecker")]
 async fn health_checker() -> impl Responder {
@@ -48,7 +53,68 @@ async fn create_task(body: Json<CreateTaskSchema>, data: Data<AppState>) -> impl
     }
 }
 
+#[get("/tasks")]
+async fn get_all_tasks(opts: Query<FilterOptions>, data: Data<AppState>) -> impl Responder {
+    let limit = opts.limit.unwrap_or(10);
+    let offset = (opts.page.unwrap_or(1) - 1) * limit;
+
+    match sqlx::query_as!(
+        TaskModel,
+        "SELECT * FROM tasks ORDER BY id LIMIT $1 OFFSET $2",
+        limit as i32,
+        offset as i32
+    )
+    .fetch_all(&data.db)
+    .await
+    {
+        Ok(tasks) => {
+            let json_response = json!({
+                "status": "success",
+                "result": tasks.len(),
+                "tasks": tasks
+            });
+
+            return HttpResponse::Ok().json(json_response);
+        }
+        Err(error) => {
+            return HttpResponse::InternalServerError().json(json!({
+                "status": "",
+                "message": format!("{:?}", error)
+            }));
+        }
+    }
+}
+
+#[get("/task/{id}")]
+async fn get_task_by_id(path: Path<Uuid>, data: Data<AppState>) -> impl Responder {
+    let task_id = path.into_inner();
+
+    match sqlx::query_as!(TaskModel, "SELECT * FROM tasks WHERE id = $1", task_id)
+        .fetch_one(&data.db)
+        .await
+    {
+        Ok(task) => {
+            let json_response = json!({
+                "status": "success",
+                "task": task
+            });
+
+            return HttpResponse::Ok().json(json_response);
+        }
+        Err(error) => {
+            return HttpResponse::InternalServerError().json(json!({
+                "status": "",
+                "message": format!("{:?}", error)
+            }));
+        }
+    }
+}
+
 pub fn config(conf: &mut ServiceConfig) {
-    let scope = scope("/api").service(health_checker).service(create_task);
+    let scope = scope("/api")
+        .service(health_checker)
+        .service(create_task)
+        .service(get_all_tasks)
+        .service(get_task_by_id);
     conf.service(scope);
 }
