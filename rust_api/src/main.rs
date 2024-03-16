5c08e54fe850922c09411d17fc66bf4b4d11a612
marques-rick <hmservicostech@outlook.com.br>
2024-03-16 12:00:49 -0300

    created post method for save new task

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -1,14 +1,49 @@
-mod services;
-mod schema;
 mod model;
+mod schema;
+mod services;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
+
+use dotenv::dotenv;
+
+use sqlx::{
+    database,
+    postgres::{PgPool, PgPoolOptions},
+    Pool, Postgres,
+};
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server is running! 🚀");
 
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must to be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("Connection DB resolved! 🚀");
+            pool
+        }
+        Err(error) => {
+            println!("Failed to connect to database 🚨: {:?}", error);
+            std::process::exit(1)
+        }
+    };
+
     HttpServer::new(move || {
-        App::new().configure(services::config)
-    }).bind(("127.0.0.1", 8080))?.run().await
+        App::new()
+            .app_data(web::Data::new(AppState { db: pool.clone() }))
+            .configure(services::config)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
