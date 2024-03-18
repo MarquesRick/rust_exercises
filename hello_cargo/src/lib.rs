cba8f6b19d1b6b463a6b5ceace8a6177ada62893
marques-rick <hmservicostech@outlook.com.br>
2024-03-17 23:46:09 -0300

    implemented project hello

--- hello_cargo/src/lib.rs
+++ hello_cargo/src/lib.rs
@@ -6,7 +6,7 @@ use std::io::{self, stdout, BufWriter};
 pub fn greet() {
     let stdout = stdout();
     let mut message_read = String::new();
-
+    println!("{:-^40}", "Begin process 1 - Say Hello!");
     println!("Type a message and Ferris will repeat it: ");
 
     io::stdin()
@@ -18,4 +18,43 @@ pub fn greet() {
     let mut writer = BufWriter::new(stdout.lock());
 
     let _ = say(&message_read, width, &mut writer);
+}
+
+pub fn calculate_numbers() {
+    let mut numbers_typed = String::new();
+    println!("{:-^40}", "Begin process 2 - Numbers!");
+    let banner = "Type a sequence numbers\n\
+                        separated by comma (,)\n\
+                        e.g: 1, 2, 3";
+    println!("{banner}");
+
+    io::stdin()
+        .read_line(&mut numbers_typed)
+        .expect("Error reading console"); //if error, return expect message
+
+    let nums: Vec<i32> = numbers_typed
+        .split(",")
+        .map(|c| c.trim().parse().expect("Error"))
+        .collect();
+
+    let result: i32 = nums.iter().sum();
+    println!("Total is: {}", result);
+}
+
+pub fn get_process_selected() {
+    let mut s = String::new();
+    println!("{:-^40}", "Begin Process");
+    println!("Type a option: (1) - Type numbers | (2) - Just say hello to Ferris");
+
+    io::stdin()
+        .read_line(&mut s)
+        .expect("Error reading console");
+
+    match s.trim().parse::<i32>().expect("Error at parsing to number") {
+        1 => calculate_numbers(),
+        2 => greet(),
+        _ => greet(),
+    }
+    println!();
+    println!("{}", "-".repeat(40));
 }
--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,10 +1,9 @@
-use hello_cargo::greet;
+use hello_cargo::get_process_selected;
 fn main() {
     //two way to do the same thing
-
     //using 'use' to import a function
-    greet();
+    get_process_selected();
     //not using 'use', passing the relative path would be:
     //(hello_cargo -> project name in Cargo.toml + function name -> greet())
-    //hello_cargo::greet();
+    //hello_cargo::get_process_selected();
 }
